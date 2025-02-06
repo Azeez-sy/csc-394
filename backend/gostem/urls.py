44535803f9fbfad518b659f68d097169c395a7ea
Dylan <96511571+justrosy@users.noreply.github.com>
2025-02-06 17:59:57 -0600

    Google OAuth

--- backend/gostem/urls.py
+++ backend/gostem/urls.py
@@ -19,6 +19,7 @@ from django.urls import path
 from django.urls import include, path
 
 urlpatterns = [
-    path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
+    path("accounts/", include("allauth.urls")),
+    path("", include("users.urls")),
 ]
