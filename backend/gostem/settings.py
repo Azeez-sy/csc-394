720fe8ade033b8437b908df370a841512df18a1e
Dallard02 <dantheguitarman02@gmail.com>
2025-02-28 21:11:29 -0600

    Daphne was duplicated in installed apps
    Deleted Extra

--- backend/gostem/settings.py
+++ backend/gostem/settings.py
@@ -34,13 +34,12 @@ SITE_ID = 3
 # Application definition
 
 INSTALLED_APPS = [
-     'daphne',
+    'daphne',
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
-    'daphne',
     "django.contrib.staticfiles",
     'rest_framework',
     'gostem',
