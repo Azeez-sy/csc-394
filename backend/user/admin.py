57157ea5051dfa0db81114f15b980864c3f2128e
Dylan <96511571+justrosy@users.noreply.github.com>
2025-03-05 13:26:04 -0600

    User List Template
    POST new users to server

--- backend/user/admin.py
+++ backend/user/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from gostem.models import User
 
 # Register your models here.
+admin.site.register(User)
--- backend/user/models.py
+++ backend/user/models.py
@@ -1,1 +1,14 @@
 from django.db import models
+
+class User(models.Model):
+    user_id     = models.AutoField(primary_key=True)
+    first_name  = models.CharField(max_length = 30)
+    last_name   = models.CharField(max_length = 30)
+    pronouns    = models.CharField(max_length = 10)
+    role        = models.CharField(max_length = 30) # roles: tutor, volunteer, admin (3), therapist 
+    prof_desc   = models.CharField(max_length = 250)
+    email       = models.EmailField(max_length = 254, unique=True)
+    isAdmin     = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return (f"self.first_name, self.last_name, self.role, self.email")
