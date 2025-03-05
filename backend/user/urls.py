078ab96b53fec9f13f3512660367085047206a4a
Dylan <96511571+justrosy@users.noreply.github.com>
2025-03-04 21:55:15 -0600

    Fixed logout
    When pressing logout button on landing page and sidebar, user will be logged out and redirected to the login page

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -1,12 +1,10 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
-from user.views import dashboard
-from user.views import get_user_profile
-
+from user.views import dashboard, get_user_profile
 
 urlpatterns = [
     #path("", views.home),
-    path("logout", views.logout_view),
-    path("dashboard/", dashboard, name="dashboard"),
+    path("logout", views.logout_view, name='logout'),
+    path("dashboard", dashboard, name="dashboard"),
     path("api/profile/", get_user_profile, name="get_user_profile"),
 ]
